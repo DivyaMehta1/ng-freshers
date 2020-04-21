3ef067ba40bfc1f9513ff9c412182fc5e0806429
DivyaMehta1 <43913340+DivyaMehta1@users.noreply.github.com>
2020-04-21 22:09:08 +0530

    Submitted Assignment

--- src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
+++ src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
@@ -1,4 +1,9 @@
+imports:[CommonModule]
+import { CommonModule } from '@angular/common';
+import { Todo, AssignmentComponent } from './../../assignment.component';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-assignment2',
@@ -6,5 +11,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./assignment2.component.css']
 })
 export class Assignment2Component {
+  todos ;
+  selectedRow : Number;
+  getClickedRow : Function;
+  constructor( arr: AssignmentComponent){
+      this.todos = arr.getTodos();
+      this.getClickedRow = function(index){
+      this.selectedRow = index; 
+  }
+  
+  // console.log(todos)
+  }
+  
 
 }
