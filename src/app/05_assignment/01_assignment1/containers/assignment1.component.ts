55840bfc4d808c3a5087a37c70559163cbd9b686
DivyaMehta1 <43913340+DivyaMehta1@users.noreply.github.com>
2020-04-21 22:05:39 +0530

    Submitted Assignments

--- src/app/05_assignment/01_assignment1/containers/assignment1.component.ts
+++ src/app/05_assignment/01_assignment1/containers/assignment1.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-assignment1',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./assignment1.component.css']
 })
 export class Assignment1Component {
+    //handler// display result via string interpolation
+    values = "";
+    onKey(value:string) {
+      this.values = value;
+    }
 
 }
